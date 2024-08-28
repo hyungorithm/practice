f1709d17344c1c9134015794601cf283fd8957c2
김성현 <shtony123@naver.com>
2024-08-28 17:53:25 +0900

    feat: item gain

--- unity/project1/Assets/Scripts/Player.cs
+++ unity/project1/Assets/Scripts/Player.cs
@@ -7,18 +7,37 @@ using Photon.Realtime;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
+
+    public int ammo;
+    public int coin;
+    public int health;
+    public int hasGrenades;
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
+
     float hAxis;
     float vAxis;
     bool wDown;
     bool jDown;
+
     bool isJump;
+    bool isDodge;
+    bool isDie;
 
     private PhotonView pv;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Rigidbody rigid;
     Animator anim;
+
+    GameObject nearObject;
 
     void Awake()
     {
@@ -36,10 +55,16 @@ public class Player : MonoBehaviour
     {
         if (pv.IsMine)
         {
+            if(health == 0)
+            {
+                if (!isDie)
+                    Die();
+            }
             GetInput();
             Move();
             Turn();
             Jump();
+            Dodge();
         }
     }
 
@@ -54,6 +79,9 @@ public class Player : MonoBehaviour
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec;
         transform.position += moveVec * speed * (wDown ? 0.6f : 2f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
@@ -67,13 +95,32 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if (jDown && !isJump)
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)
         {
             rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
             isJump = true;
         }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -83,5 +130,42 @@ public class Player : MonoBehaviour
             anim.SetBool("isJump", false);
             isJump = false;
         }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if (ammo > maxAmmo)
+                        ammo = maxAmmo;
+                    break;
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if (coin > maxCoin)
+                        coin = maxCoin;
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+                case Item.Type.Grenade:
+                    hasGrenades += item.value;
+                    if (hasGrenades > maxHasGrenades)
+                        hasGrenades = maxHasGrenades;
+                    break;
+            }
+            Destroy(other.gameObject);
+        }
+    }
+
+    void Die()
+    {
+        isDie = true;
     }
 }
